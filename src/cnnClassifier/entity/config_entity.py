c8e2110e0912104fcd8e2f108dcc976358fe7ca1
“venkatshan707” <venkatshan707@gmail.com>
2023-11-30 23:00:43 +0530

    Preparing base model module

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -10,6 +10,18 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
-
     # DataIngestionConfig is an entity. if one function defined with return type as this entity,
     #  that function should return 4 variable values (with data type defined) as mentioned in entity
+
+
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weights: str
+    params_classes: int
